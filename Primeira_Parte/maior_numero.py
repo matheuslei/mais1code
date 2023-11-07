8082233c13c0010bd8bab27f652a9f09a481fdf9
Matheus Leite de Castro <65515537+matheuslei@users.noreply.github.com>
2023-11-06 21:13:35 -0300

    Update maior_numero.py

--- Primeira_Parte/maior_numero.py
+++ Primeira_Parte/maior_numero.py
@@ -2,6 +2,11 @@ num1 = int(input("Digite o primeiro número: "))
 num2 = int(input("Digite o segundo número: "))
 num3 = int(input("Digite o terceiro número: "))
 
-maior_numero = max(num1, num2, num3)
+maior_numero = num1
+
+if num2 > maior_numero:
+    maior_numero = num2
+if num3 > maior_numero:
+    maior_numero = num3
 
 print(f"O maior número é: {maior_numero}")
