a35d75719592a595c98e2a2aacfc26f46ec47875
Matheus Leite de Castro <65515537+matheuslei@users.noreply.github.com>
2023-11-14 12:46:11 -0300

    Create bissexto.py

---
+++ Quarta_Parte/bissexto.py
@@ -0,0 +1,9 @@
+ano = int(input("Digite um ano: "))
+
+if ano % 4 == 0:
+    if ano % 100 != 0 or ano % 400 == 0:
+        print(f"O ano {ano} é bissexto.")
+    else:
+        print(f"O ano {ano} não é bissexto.")
+else:
+    print(f"O ano {ano} não é bissexto.")
