d0771fb4e8c0f473d94036b866ca10d4f5e035d6
Matheus Leite de Castro <65515537+matheuslei@users.noreply.github.com>
2023-11-14 12:55:29 -0300

    Create fila_de_banco.py

---
+++ Quinta_Parte/fila_de_banco.py
@@ -0,0 +1,7 @@
+fila = ["Pessoa 1", "Pessoa 2", "Pessoa 3", "Pessoa 4", "Pessoa 5", "Pessoa 6", "Pessoa 7", "Pessoa 8", "Pessoa 9", "Pessoa 10"]
+
+# Atender as duas primeiras pessoas
+fila.pop(0)
+fila.pop(0)
+
+print(fila)
