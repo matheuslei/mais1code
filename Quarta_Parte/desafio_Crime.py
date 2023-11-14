d52379682bd77de289b200768c9f45f7ff43d111
Matheus Leite de Castro <65515537+matheuslei@users.noreply.github.com>
2023-11-14 12:50:29 -0300

    Create desafio_Crime.py

---
+++ Quarta_Parte/desafio_Crime.py
@@ -0,0 +1,25 @@
+perguntas = [
+    "Telefonou para a vítima?",
+    "Esteve no local do crime?",
+    "Mora perto da vítima?",
+    "Devia para a vítima?",
+    "Já trabalhou com a vítima?"
+]
+
+respostas_positivas = 0
+
+for pergunta in perguntas:
+    resposta = input(pergunta + " (sim/não): ")
+    if resposta.lower() == "sim":
+        respostas_positivas += 1
+
+if respostas_positivas == 2:
+    classificacao = "Suspeita"
+elif 3 <= respostas_positivas <= 4:
+    classificacao = "Cúmplice"
+elif respostas_positivas == 5:
+    classificacao = "Assassino"
+else:
+    classificacao = "Inocente"
+
+print(f"Classificação: {classificacao}")
